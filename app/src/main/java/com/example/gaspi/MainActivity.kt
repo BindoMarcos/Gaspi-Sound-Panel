3bb2b2418896d9a1a985c525c9a5857a243fdfd5
BindoMarcos <Bindomarcos13@gmail.com>
2021-08-16 18:06:42 -0300

    Cambios: -Se quito los OnclickListener de la funcion
    -Se aplico kotlin-android-extensions
    
    +Botones de sonido inicializados en Array
    
    -Agregadas Ads Intersiciales

--- app/src/main/java/com/example/gaspi/MainActivity.kt
+++ app/src/main/java/com/example/gaspi/MainActivity.kt
@@ -9,11 +9,12 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.ads.*
 import com.google.android.gms.ads.interstitial.InterstitialAd
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
     private var adIntersicial: InterstitialAd? = null
-    private var TAG = "INTAD"
+    private var TAG = "IntAd"
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -26,7 +27,7 @@ class MainActivity : AppCompatActivity() {
         adMain.loadAd(adRequest)
         //value final: ca-app-pub-5766982615688532/1985753795
         //Value Prueba: ca-app-pub-3940256099942544/1033173712
-        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712" ,adRequest, object : InterstitialAdLoadCallback() {
+        InterstitialAd.load(this,"ca-app-pub-5766982615688532/1985753795" ,adRequest, object : InterstitialAdLoadCallback() {
             override fun onAdFailedToLoad(adError: LoadAdError) {
                 Log.d(TAG, adError.message)
                 adIntersicial = null
@@ -46,194 +47,186 @@ class MainActivity : AppCompatActivity() {
             override fun onAdFailedToShowFullScreenContent(adError: AdError?) {
                 Log.d(TAG, "Ad failed to show.")
             }
-
             override fun onAdShowedFullScreenContent() {
                 Log.d(TAG, "Ad showed fullscreen content.")
                 adIntersicial = null
             }
         }
 
+        val botones = arrayOf(bFiumba,bVamosArriba,bDespertarse,bBoliviano,bBuenas,bBuenosDias,bTePego,bLindaManana,
+            bChumba,bAnibalFernandez,bComoTeVa,bMuro1,bFernanflo1,bFernanflo2,bFinalFeliz,bGerman,bPL,bPLTrans,bParaguayo,
+            bBuenasATodos,bMuro2,bPES1,bPES2,bQuinciañera,bSambabam,bSilvia,bSilvina,bSiuk,bSocrates,bSub,bSuculini,bTrans)
 
-        val buenas = findViewById<Button>(R.id.bBuenas)
-        val fiumba =findViewById<Button>(R.id.bFiumba)
-        val Despertarse= findViewById<Button>(R.id.bDespertarse)
-        val Boliviano= findViewById<Button>(R.id.bBoliviano)
-        val BuenosDias= findViewById<Button>(R.id.bBuenosDias)
-        val LindaManana= findViewById<Button>(R.id.bLindaMañana)
-        val TePego = findViewById<Button>(R.id.bTePego)
-        val VamosArriba= findViewById<Button>(R.id.bVamosArriba)
-        val Anibal=findViewById<Button>(R.id.bAnibalFernandez)
-        val Chumba = findViewById<Button>(R.id.bChumba)
-        val ComoTeVa=findViewById<Button>(R.id.bComoTeVa)
-        val Muro1=findViewById<Button>(R.id.bMuro1)
-        val Muro2=findViewById<Button>(R.id.bMuro2)
-        val Fernanflo1=findViewById<Button>(R.id.bFernanflo1)
-        val Fernanflo2 =findViewById<Button>(R.id.bFernanflo2)
-        val German = findViewById<Button>(R.id.bGerman)
-        val FinalFeliz= findViewById<Button>(R.id.bFinalFeliz)
-        val PL = findViewById<Button>(R.id.bPL)
-        val PLTrans = findViewById<Button>(R.id.bPLTrans)
-        val BuenasATodos = findViewById<Button>(R.id.bBuenasATodos)
-        val Paraguayo = findViewById<Button>(R.id.bParaguayo)
-        val Pes1 = findViewById<Button>(R.id.bPES1)
-        val Pes2 = findViewById<Button>(R.id.bPES2)
-        val Quiencianera = findViewById<Button>(R.id.bQuinciañera)
-        val Sambabam = findViewById<Button>(R.id.bSambabam)
-        val Silvia = findViewById<Button>(R.id.bSilvia)
-        val Silvina = findViewById<Button>(R.id.bSilvina)
-        val Siuk = findViewById<Button>(R.id.bSiuk)
-        val Socrates = findViewById<Button>(R.id.bSocrates)
-        val Suscribite = findViewById<Button>(R.id.bSub)
-        val Suculini = findViewById<Button>(R.id.bSuculini)
-        val Trans= findViewById<Button>(R.id.bTrans)
-        val BotonRedes= findViewById<Button>(R.id.bRedes)
+        val botonRedes= findViewById<Button>(R.id.bRedes)
 
-        fun Botonera() {
+        /* Lista numeros botones
+        buenas = 0
+         fiumba = 1
+         Despertarse= 2
+         Boliviano= 3
+         BuenosDias= 4
+         LindaManana= 5
+         TePego = 6
+         VamosArriba= 7
+         Anibal= 8
+         Chumba = 9
+         ComoTeVa= 10
+         Muro1= 11
+         Muro2= 12
+         Fernanflo1= 13
+         Fernanflo2 = 14
+         German = 15
+         FinalFeliz= 16
+         PL = 17
+         PLTrans = 18
+         BuenasATodos = 19
+         Paraguayo = 20
+         Pes1 = 21
+         Pes2 = 22
+         Quiencianera = 23
+         Sambabam = 24
+         Silvia = 25
+         Silvina = 26
+         Siuk = 27
+         Socrates = 28
+         Suscribite = 29
+         Suculini = 30
+         Trans= 31*/
 
-            buenas.setOnClickListener {
-                val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.buenas)
-                mediaPlayer?.start()
-            }
-
-            fiumba.setOnClickListener {
+            botones[0].setOnClickListener {
                 val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.fiumba)
                 mediaPlayer?.start()
             }
-
-            Despertarse.setOnClickListener{
-                val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.a_despertarse)
-                mediaPlayer?.start()
-            }
-
-            Boliviano.setOnClickListener{
-                val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.boliviano)
-                mediaPlayer?.start()
-            }
-
-            BuenosDias.setOnClickListener{
-                val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.buenos_dias)
-                mediaPlayer?.start()
-            }
-
-            LindaManana.setOnClickListener{
-                val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.linda_manana)
-                mediaPlayer?.start()
-            }
-
-            TePego.setOnClickListener{
-                val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.te_pego)
-                mediaPlayer?.start()
-            }
-
-            VamosArriba.setOnClickListener {
+            botones[1].setOnClickListener {
                 val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.vamos_arriba)
                 mediaPlayer?.start()
             }
-
-            Anibal.setOnClickListener {
-                val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.anibal_fernandez)
+            botones[2].setOnClickListener{
+                val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.a_despertarse)
                 mediaPlayer?.start()
             }
-            Chumba.setOnClickListener {
+            botones[3].setOnClickListener{
+                val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.boliviano)
+                mediaPlayer?.start()
+            }
+            botones[4].setOnClickListener {
+                val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.buenas)
+                mediaPlayer?.start()
+            }
+            botones[5].setOnClickListener{
+                val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.buenos_dias)
+                mediaPlayer?.start()
+            }
+            botones[6].setOnClickListener{
+                val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.te_pego)
+                mediaPlayer?.start()
+            }
+            botones[7].setOnClickListener{
+                val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.linda_manana)
+                mediaPlayer?.start()
+            }
+            botones[8].setOnClickListener {
                 val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.chumba)
                 mediaPlayer?.start()
             }
-            ComoTeVa.setOnClickListener {
+            botones[9].setOnClickListener {
+                val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.anibal_fernandez)
+                mediaPlayer?.start()
+            }
+            botones[10].setOnClickListener {
                 val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.como_te_va)
                 mediaPlayer?.start()
             }
-            Muro1.setOnClickListener {
+            botones[11].setOnClickListener {
                 val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.el_muro_infernal)
                 mediaPlayer?.start()
             }
-            Muro2.setOnClickListener{
-                val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.muro_infernal_2)
-                mediaPlayer?.start()
-            }
-            Fernanflo1.setOnClickListener {
+            botones[12].setOnClickListener {
                 val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.fernanflo)
                 mediaPlayer?.start()
             }
-            Fernanflo2.setOnClickListener {
+            botones[13].setOnClickListener {
                 val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.fernanflo_2)
                 mediaPlayer?.start()
             }
-            German.setOnClickListener {
-                val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.german_garmendia)
-                mediaPlayer?.start()
-            }
-            FinalFeliz.setOnClickListener {
+            botones[14].setOnClickListener {
                 val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.final_feliz)
                 mediaPlayer?.start()
             }
-            PL.setOnClickListener {
+            botones[15].setOnClickListener {
+                val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.german_garmendia)
+                mediaPlayer?.start()
+            }
+            botones[16].setOnClickListener {
                 val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.paulo_londra)
                 mediaPlayer?.start()
             }
-            PLTrans.setOnClickListener {
+            botones[17].setOnClickListener {
                 val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.paulo_londra_trans)
                 mediaPlayer?.start()
             }
-            BuenasATodos.setOnClickListener {
-                val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.muy_buenas_a_todos)
-                mediaPlayer?.start()
-            }
-            Paraguayo.setOnClickListener {
+            botones[18].setOnClickListener {
                 val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.paraguayo)
                 mediaPlayer?.start()
             }
-            Pes1.setOnClickListener {
+            botones[19].setOnClickListener {
+                val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.muy_buenas_a_todos)
+                mediaPlayer?.start()
+            }
+            botones[20].setOnClickListener{
+                val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.muro_infernal_2)
+                mediaPlayer?.start()
+            }
+            botones[21].setOnClickListener {
                 val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.pro_evolution_soccer)
                 mediaPlayer?.start()
             }
-            Pes2.setOnClickListener {
+            botones[22].setOnClickListener {
                 val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.pro_evolution_soccer_2)
                 mediaPlayer?.start()
             }
-            Quiencianera.setOnClickListener {
+            botones[23].setOnClickListener {
                 val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.quincianera)
                 mediaPlayer?.start()
             }
-            Sambabam.setOnClickListener {
+            botones[24].setOnClickListener {
                 val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.sambabam)
                 mediaPlayer?.start()
             }
-            Silvia.setOnClickListener {
+            botones[25].setOnClickListener {
                 val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.silvia)
                 mediaPlayer?.start()
             }
-            Silvina.setOnClickListener {
+            botones[26].setOnClickListener {
                 val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.silvina_escudero)
                 mediaPlayer?.start()
             }
-            Siuk.setOnClickListener {
+            botones[27].setOnClickListener {
                 val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.siuk)
                 mediaPlayer?.start()
             }
-            Socrates.setOnClickListener {
+            botones[28].setOnClickListener {
                 val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.socrates)
                 mediaPlayer?.start()
             }
-            Suscribite.setOnClickListener {
+            botones[29].setOnClickListener {
                 val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.sub_a_la_p)
                 mediaPlayer?.start()
             }
-            Suculini.setOnClickListener {
+            botones[30].setOnClickListener {
                 val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.suculini)
                 mediaPlayer?.start()
             }
-            Trans.setOnClickListener {
+            botones[31].setOnClickListener {
                 val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.transexual)
                 mediaPlayer?.start()
             }
 
-            BotonRedes.setOnClickListener {
+            botonRedes.setOnClickListener {
                 val intent = Intent(this, Redes::class.java)
                 startActivity(intent)
             }
         }
-        Botonera()
-    }
 
     override fun onResume() {
         super.onResume()
